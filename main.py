406338fe970177005c183721191bc39001887ff5
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2025-03-19 22:55:55 +0100

    replace whatsapp bot with web server

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 import machine
 
 from wifi_controller import WifiController
-from bot_controller import WhatsappBot
+from pico_controller import PicoController
+from webserver import WebServer
 
 
 if __name__ == "__main__":
     ssid = 'IPhone Kyrylo'
     password = 'Kempel7654'
-#    api_key = '2568235'
-    api_key = 'EVgBe9DPNRpE'
-    phone = '420728372280'
     
     # init controllers
     wifi = WifiController(ssid, password)
-    whatsapp_bot = WhatsappBot(api_key, phone)
+    pico = PicoController()
     
     # try to establish the wifi connection
     if not wifi.connect():
@@ -21,10 +19,9 @@ if __name__ == "__main__":
         raise Exception()
     else:
         print(":D\n")
-        print("Sending message...")
-        msg = 'Hello from the Raspberry Pi Pico'
-        whatsapp_bot.send_message(msg)
+        server = WebServer('172.20.10.2', pico=pico)
         
+        server.run()
         
     
     
