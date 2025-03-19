8b495138a1b15beff8cc79baa2205c70e72b414a
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2025-03-19 22:04:35 +0100

    change bot API

--- bot_controller.py
+++ bot_controller.py
@@ -1,25 +1,36 @@
-import requests
+import urequests
 
 
 class WhatsappBot:
     
-    def __init__(self, api_key: str):
+    def __init__(self, api_key: str, phone_number: str):
         self._api_key = api_key
-        self._url = lambda phone, message: f'https://api.callmebot.com/whatsapp.php?phone={phone}&text={message}&apikey={self._api_key}'
+        self._dest_phone = phone_number
         
-    def send_message(self, dest_phone_number: str, message: str):
+    def send_message(self, message: str):
         # send a GET request on whatsapp API endpoint
-        response = urequests.get(self._url(dest_phone_number, message))
+        prepared_msg = self._prepare_message(message)
+        url = f'http://api.textmebot.com/send.php?recipient={self._dest_phone}&apikey={self._api_key}&text={prepared_msg}'
+        response = urequests.get(url)
         
         # check if request was successful
         if response.status_code == 200:
             print("Success!")
         else:
             print("Error:")
-            print(response.text)
-
+            print(f"URL: {url}")
+            print(response.text)            
+            
+    def _prepare_message(self, message: str) -> str:
+        split_symbol = '%20'
+        end_symbol = '%21'
+        prepared_msg = message.replace(' ', split_symbol) + end_symbol
+        return prepared_msg
 
 
 if __name__ == "__main__":
-    bot = WhatsappBot('2568235')
-    bot.send_message('420728372278', 'hello')
+    bot = WhatsappBot('2568235', '420728372280')
+    bot.send_message('Hello my name is Kyrylo')
+#    res = bot.prepare_message('Hello my name is Kyrylo')
+#    print(res)
+
--- main.py
+++ main.py
@@ -7,21 +7,24 @@ from bot_controller import WhatsappBot
 if __name__ == "__main__":
     ssid = 'IPhone Kyrylo'
     password = 'Kempel7654'
-    api_key = '2568235'
+#    api_key = '2568235'
+    api_key = 'EVgBe9DPNRpE'
     phone = '420728372280'
     
     # init controllers
     wifi = WifiController(ssid, password)
-    whatsapp_bot = WhatsappBot(api_key)
+    whatsapp_bot = WhatsappBot(api_key, phone)
     
     # try to establish the wifi connection
     if not wifi.connect():
         print("Not connected")
         raise Exception()
     else:
-        print(":D")
+        print(":D\n")
         print("Sending message...")
-        msg = 'Hello%20from%20the%20Raspberry%20Pi%20Pico%21'
-        whatsapp_bot.send_message(phone, msg)
+        msg = 'Hello from the Raspberry Pi Pico'
+        whatsapp_bot.send_message(msg)
+        
+        
     
     
