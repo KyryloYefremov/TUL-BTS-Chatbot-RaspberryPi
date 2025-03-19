f07f87de944493031e800493d87534b41fb99662
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2025-03-19 11:32:39 +0100

    add test message sending

--- main.py
+++ main.py
@@ -5,19 +5,23 @@ from bot_controller import WhatsappBot
 
 
 if __name__ == "__main__":
-    ssid = "IPhone Kyrylo"
-    password = "Kempel7654"
-    api_key = "2568235"
-    phone = "420728372280"
+    ssid = 'IPhone Kyrylo'
+    password = 'Kempel7654'
+    api_key = '2568235'
+    phone = '420728372280'
     
+    # init controllers
     wifi = WifiController(ssid, password)
     whatsapp_bot = WhatsappBot(api_key)
     
-    # TODO: fix the wifi connection part
+    # try to establish the wifi connection
     if not wifi.connect():
         print("Not connected")
         raise Exception()
     else:
         print(":D")
+        print("Sending message...")
+        msg = 'Hello%20from%20the%20Raspberry%20Pi%20Pico%21'
+        whatsapp_bot.send_message(phone, msg)
     
     
