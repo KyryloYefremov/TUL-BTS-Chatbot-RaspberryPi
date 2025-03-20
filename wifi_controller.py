8bc4af76a1741eb4e981fe0b8bf9d53f070ffa68
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2025-03-20 21:32:50 +0100

    add func to get wlan network props

--- wifi_controller.py
+++ wifi_controller.py
@@ -9,24 +9,29 @@ class WifiController:
         self._password = password
         
     def connect(self, connection_timeout=10) -> bool:
-        wlan = network.WLAN(network.STA_IF)
-        wlan.active(True)
+        self._wlan = network.WLAN(network.STA_IF)
+        self._wlan.active(True)
         # connect to your network
-        wlan.connect(self._ssid, self._password)
+        self._wlan.connect(self._ssid, self._password)
         
         # wait for Wi-Fi connection
         while connection_timeout > 0:
-            if wlan.status() >= 3:
+            if self._wlan.status() >= 3:
                 break
             connection_timeout -= 1
             print('Waiting for Wi-Fi connection...')
             sleep(1)
             
         # check if connection is successful
-        if wlan.status() != 3:
+        if self._wlan.status() != 3:
             return False
         else:
             print('Connection successful!')
-            network_info = wlan.ifconfig()
-            print('IP address:', network_info[0])
+            network_info = self.ipconfig()
+            print(network_info)
             return True
+        
+    def ipconfig(self):
+        ip, subnet_mask, gateway, dns = self._wlan.ifconfig()
+        info = f"Network ssid: {self._ssid}\nIP: {ip}\nSubnet mask: {subnet_mask}\nGateway: {gateway}\nDns: {dns}\n"
+        return info
