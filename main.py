70b0dedbe6c83f98088156533968fb8c8680eda6
Kyrylo_Yefremov <kyrylo.yefremov@tul.cz>
2025-03-20 21:33:20 +0100

    final version of program

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from wifi_controller import WifiController
 from pico_controller import PicoController
 from webserver import WebServer
 
+from config import *
+
 
 if __name__ == "__main__":
-    ssid = 'IPhone Kyrylo'
-    password = 'Kempel7654'
-    
+
     # init controllers
-    wifi = WifiController(ssid, password)
+    wifi = WifiController(SSID, PASSWORD)
     pico = PicoController()
+    
+    # show that program was executed - blink the led
+    pico.init_blink()
     
     # try to establish the wifi connection
     if not wifi.connect():
@@ -19,7 +22,7 @@ if __name__ == "__main__":
         raise Exception()
     else:
         print(":D\n")
-        server = WebServer('172.20.10.2', pico=pico)
+        server = WebServer('172.20.10.2', pico=pico, wifi=wifi, weather_api=WEATHER_API)
         
         server.run()
         
